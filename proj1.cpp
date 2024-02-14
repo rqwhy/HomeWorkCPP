76fd492fa14ee076ae0142be9e71be175bca7286
Arseniy Babahov <157617055+rqwhy@users.noreply.github.com>
2024-02-14 18:39:11 +0300

    Add files via upload

---
+++ proj1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    cout << "Если жизнь тебя обманет," <<endl << "Не печалься, не сердись!" << endl << "В день уныния смирись:" << endl << "День веселья, верь, настанет." << endl << "Сердце в будущем живет;" << endl << "Настоящее уныло:" << endl << "Все мгновенно, все пройдет;" << endl << "Что пройдет, то будет мило." << endl;
+    cout << " \t\t \"А.С.Пушкин\" " <<endl;
+
+
+    cout << "Каждый" <<endl;
+    cout << "\tОхотник" << endl;
+    cout << "\t\tЖелает" << endl;
+    cout << "\t\t\tЗнать" << endl;
+    cout << "\t\t\t\tГде" << endl;
+    cout << "\t\t\t\t\tСидит" << endl;
+    cout << "\t\t\t\t\t\tФазан" << endl;
+
+    cout << "Продам щенков." << endl;
+    cout << "Порода:Ротвейлер." << endl;
+    cout << "..." << endl;
+    cout << "Тел.: 222-22-22" << endl;
+    cout << "222 \t" << char(124) << " 222" << char(124) << endl << "22  \t" << char(124) << " 22 " << char(124) << endl << "22  \t" << char(124) << " 22 " << char(124) << endl <<endl << "и.т.д.";
+
+
+    
+}
